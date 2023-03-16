e5e4baf8e40d7e46cdd99fe5d25fde32f090b3f9
peter <peterekwere04@gmail.com>
2023-03-16 16:57:29 -0700

    created program for task 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main-entry point
+ * main-prints \"Programming is like building a multilingual puzzle
  *
  *  Return:Always 0 success
  */
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main- entry point
+ * main- Prints the size of various types based on
+ *  *        the computer it is compiled and run on.
  *
  * Return:Always 0 success
  */
