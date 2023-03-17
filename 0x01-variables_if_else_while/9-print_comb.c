2e2f0ff9801a8421dd4133e620c23cf42b7bde4e
peter <peterekwere04@gmail.com>
2023-03-17 15:55:58 -0700

    updated programs for task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,19 +9,19 @@
 
 int main(void)
 {
-	int ch;
-		for (ch = 48; ch < 58; ch++)
-	{
-		if (ch == 57)
-		{
-			putchar(ch);
-			break;
-		}
-		else
-		{
-			putchar(ch);
-			putchar(',');
-		}
-	} putchar('\n');
-	return (0);
+        int ch;
+                for (ch = 48; ch < 58; ch++)
+        {
+                if (ch == 57)
+                {
+                        putchar(ch);
+                        break;
+                }
+                else
+                {
+                        putchar(ch);
+                        putchar(',');
+                        putchar(' ');
+                }
+        } return (0);
 }
