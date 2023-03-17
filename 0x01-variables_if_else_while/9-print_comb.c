c18d532ddc275415c56e0b19bd17dd9cc5037507
peter <peterekwere04@gmail.com>
2023-03-17 15:45:55 -0700

    created programs for task 8 and 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - entry point
+*
+*Return:Always 0 success
+*/
+
+int main(void)
+{
+	int ch;
+		for (ch = 48; ch < 58; ch++)
+	{
+		if (ch == 57)
+		{
+			putchar(ch);
+			break;
+		}
+		else
+		{
+			putchar(ch);
+			putchar(',');
+		}
+	} putchar('\n');
+	return (0);
+}
