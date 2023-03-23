2cfcb64753a83f76622b37eaded37a25d55e2d29
peter <peterekwere04@gmail.com>
2023-03-23 04:02:34 -0700

    updated the 1-main.c program

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,11 +11,14 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
+
+/* Description: This  is a buggy while loop that will cause an infinty loop. */
 /*	i = 0; */
 /*	 while (i < 10) */
 /*	{ */
 /*              putchar(i); */
 /*      } */
+
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
