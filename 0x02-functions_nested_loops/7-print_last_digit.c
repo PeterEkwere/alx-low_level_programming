208c874971f713aa1f5f51b188e191ee038e9435
peter <peterekwere04@gmail.com>
2023-03-25 01:19:43 -0700

    updated program for task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,7 @@ int print_last_digit(int c)
 {
 	int num = _abs(c);
 	int last_digit = num % 10;
-
-		_putchar(last_digit + '0');
-	return (last_digit);
+	
+	_putchar(last_digit + '0');
+	return (0);
 }
