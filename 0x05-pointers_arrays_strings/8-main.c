e83dd7af5cb6861836f7ca551e4d04f184a53754
peter <peterekwere04@gmail.com>
2023-03-28 07:24:10 -0700

    created program for task 8

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[7];
+
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    array[5] = 444;
+    array[6] = -909;
+    print_array(array, 5);
+    return (0);
+}
