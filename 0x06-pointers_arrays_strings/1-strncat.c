687a587a4d8388147b1c620c4729a0f34836597b
peter <peterekwere04@gmail.com>
2023-03-30 05:48:19 -0700

    created program for task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,10 +3,10 @@
 /**
  * _strncat - is a function that append and array with another array
  * @src: is a pointer to an array
- * @dest:is a pointer to an array 
+ * @dest:is a pointer to an array
  * @n:is also an integer
  * Return: it will return an the pointer to the destination array
- * */ 
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
@@ -17,13 +17,13 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-	
+
 	while ((j < n) && (*(src + j) != '\0'))
 	{
 		*(dest + i) = *(src + j);
 		i++;
 		j++;
-	} return dest;
+	} return (dest);
 }
 
 
