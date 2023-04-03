1cf703943a75bd8407bfe4378737db0b3d2704ec
peter <peterekwere04@gmail.com>
2023-04-03 14:07:14 -0700

    created program for task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,7 +17,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; *(haystack + i) != '\0'; i++)
 	{
-		for (j = 0; *(needle + j) != '\0' && haystack[i + j] == needle [j]; j++)
+		for (j = 0; *(needle + j) != '\0' && haystack[i + j] == needle[j]; j++)
 		{
 			if (*(haystack + i) == *(needle + 0))
 			{
