743601eec66b642a9b7fca4b6345253a834a6daa
peter <peterekwere04@gmail.com>
2023-04-03 13:53:09 -0700

    created program for task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strstr - used to calculate the length of the initial substring of a string
+ * @haystack: is a pointer to a string to be searched
+ * @needle:is a set of characters we want to search for
+ * Return: return  a char pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	size_t i;
+
+	for (i = 0; *(haystack + i) != '\0'; i++)
+	{
+		if (*(haystack + i) == *(needle + 0))
+		{
+			return (haystack + i);
+		}
+	} return (NULL);
+}
