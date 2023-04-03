6aef820c84acbf2495cbf6f31abfe84b5f179012
peter <peterekwere04@gmail.com>
2023-04-03 07:12:33 -0700

    created program for task 2

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 char *leet(char *);
 char *cap_string(char *j);
 char *string_toupper(char *j);
