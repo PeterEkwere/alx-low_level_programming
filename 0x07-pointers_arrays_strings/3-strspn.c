36aa23dbadf7bfa34044797a604a63e00d505b62
peter <peterekwere04@gmail.com>
2023-04-03 12:45:13 -0700

    fixed betty error for task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,7 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 	size_t j;
 	unsigned int count = 0;
 
-	for(i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
