9158fbe9f0bb6cdc4e5da8d8c9e49adfc92e01d0
peter <peterekwere04@gmail.com>
2023-04-03 13:35:59 -0700

    created program for task 4

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
