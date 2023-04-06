480fb309c4258f8d84068e6aff9b100be37a5132
peter <peterekwere04@gmail.com>
2023-04-05 19:41:24 -0700

    fixed task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,23 +8,25 @@
 void print_triangle(int size)
 {
 	int i;
-	int j;
 	int k;
 
-	if (size == 0)
+	if (size > 0)
 	{
-		_putchar('\n');
-	}
-
-	for (i = 1; i < size + 1; i++)
-	{
-		for (k = 0; k <	size - i ; k++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar(' ');
+			for (k = size - i; k > 0; k--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; j < i; k++)
+			{
+				_putchar('#');
+			}
+			if (hash == size)
+			{
+				continue;
+			} _putchar('\n');
 		}
-		for (j = 0; j < i; j++)
-		{
-			_putchar('#');
-		} _putchar('\n');
 	}
+	_putchar('\n');
 }
