6c5599fd68d007086480f29a54253fa88d7f5745
peter <peterekwere04@gmail.com>
2023-04-05 19:12:38 -0700

    created program for task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,20 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	size_t i = 0;
-	size_t j = 0;
-
-	if (*(s + i) == '\0')
+	if (*s == '\0')
 	{
 		return;
-	}
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-	while  (i > j)
-	{
-		_putchar(*(s + i - 1));
-		i--;
-	} _print_rev_recursion(s - 1);
+	}  _print_rev_recursion(s + 1);
+	_putchar(*(s));
 }
