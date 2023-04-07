782a1feb02c3675309dfaf5ef988e97f2e481d3d
peter <peterekwere04@gmail.com>
2023-04-07 11:23:04 -0700

    created program for task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,5 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
  * main - point of entry
  * @argc: is the argument count
  * @argv: is the argument vector
@@ -18,13 +7,8 @@ int _putchar(char c)
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
 	if (argc == 1)
 	{
-		for (i = 0; argv[0][i] != '\0'; i++)
-		{
-			_putchar(argv[0][i]);
-		} _putchar('\n');
+		puts(argv[0]);
 	} return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <unistd.h>
 
 int is_palindrome(char *s);
 int _sqrt_recursion(int n);
