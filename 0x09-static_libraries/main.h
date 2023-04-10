e06d3fa05a1a6b04da17404899dff4170a659c72
peter <peterekwere04@gmail.com>
2023-04-10 13:10:43 -0700

    created the static library

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,51 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int is_palindrome(char *s);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
+void set_string(char **s, char *to);
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+char *leet(char *);
+char *cap_string(char *j);
+char *string_toupper(char *j);
+void reverse_array(int *a, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+void print_rev(char *s);
+void _puts(char *str);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+
+
+#endif /* MAIN_H */
