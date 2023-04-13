918c6173eedebdccc3a44d75872ce35aa2f15b0a
peter <peterekwere04@gmail.com>
2023-04-13 14:39:25 -0700

    created program for task  1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,11 +14,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		s1 = '\0';
+		s1 = "";
 	}
 	else if (s2 == NULL)
 	{
-		s2 = '\0';
+		s2 = "";
 	}
 	while (s1[i] != '\0')
 	{
