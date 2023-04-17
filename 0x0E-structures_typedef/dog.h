6b16103fc527a223c7a5cfb0ede0946f7f170d6e
peter <peterekwere04@gmail.com>
2023-04-17 03:21:40 -0700

    created struct for task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - is a structure
+ * @name: is the name of the dog
+ * @age: is the age of the dog
+ * @owner: is the owner of the dog
+ * Return: void;
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
