5ff6254782ef1726c6f41931f04acd1b43a7ab5e
peter <peterekwere04@gmail.com>
2023-04-27 08:18:58 -0700

    created a program for task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - is a function that prints all the elements of a list
+ * @h: is the first node
+ * Return: the amount of all the elements of a list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int len = 0;
+	char *s;
+	size_t count = 0;
+
+
+	while (h != NULL)
+	{
+		len = h->len;
+		s = h->str;
+
+		printf("[%i] %s \n", len, s);
+		count++;
+
+		h = h->next;
+	}
+	return (count);
+
+}
