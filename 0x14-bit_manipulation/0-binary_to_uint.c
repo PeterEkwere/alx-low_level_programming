bb5b962fde122600c5055ed52559229b923f6aed
peter <peterekwere04@gmail.com>
2023-05-04 15:50:58 -0700

    created the function for task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * binary_to_int - is a function that converts 
+ * a binary digit to unary digit
+ *
+ * @b: is a pointer to an array of char binary
+ * Return: an  unsigned int if true and 0 if false
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int un;
+	unsigned int i = 0;
+	unsigned int result = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '1')
+		{
+			un = (result << 1) | 1;
+			result = un;
+		}
+		else if (b[i] == '0')
+		{
+			un = (result << 1);
+			result = un;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	return (result);
+}
