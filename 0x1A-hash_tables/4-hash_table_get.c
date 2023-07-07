edeb019cc585d02efd65a839224a255101781275
PeterEkwere <peterekwere04@gmail.com>
2023-07-07 10:25:21 -0700

    added edge case to check for empty table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,7 +9,14 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	unsigned long int index;
+
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+
+	index = key_index((unsigned char *)key, ht->size);
 
 	if (ht->array[index] == NULL)
 	{
